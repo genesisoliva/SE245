dac0a654e2e8616ffc78dbf743c535b075fa2ef9
genesisoliva <42148976+genesisoliva@users.noreply.github.com>
2021-05-26 12:04:52 -0400

    Add files via upload

---
+++ LAB7-GENESIS/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB6_GENESIS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPerson_Click(object sender, EventArgs e)
+        {
+            PersonV2 temp = new PersonV2();
+            temp.Feedback = "";
+
+            temp.FirstName = txtFirstName.Text;
+            temp.MiddleName = txtMiddleName.Text;
+            temp.LastName = txtLastName.Text;
+
+            temp.Street1 = txtStreet.Text;
+            temp.City = txtCity.Text;
+            temp.State = txtState.Text;
+            temp.ZipCode = txtZipCode.Text;
+
+            temp.Street2 = txtStreet2.Text;
+
+            temp.PhoneNumber = txtPhoneNumber.Text;
+            temp.Email = txtEmail.Text;
+
+            temp.CellPhone = txtCellPhone.Text;
+            temp.Instagram = txtInstagram.Text;
+
+            if(temp.Feedback.Contains("ERROR:"))
+            {
+                lblFeedback.Text = temp.AddAPersonRecord();
+            }
+            else
+            {
+                lblFeedback.Text = $"\n{temp.FirstName} {temp.MiddleName} {temp.LastName}\n\nADDRESS INFORMATION:\n{temp.Street1}\n{temp.City} {temp.State} {temp.ZipCode}\n\n{temp.Street2}\n\nCONTACT INFORMATION:\n\n{temp.PhoneNumber} (Phone Number)\n{ temp.CellPhone} (Cell Phone)\n{ temp.Email}";
+            }
+        }
+
+        
+    }
+}
---
+++ LAB7-GENESIS/Person.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB6_GENESIS
+{
+    class Person
+    {
+        private string firstname;
+        private string middlename;
+        private string lastname;
+        private string street1;
+        private string city;
+        private string state;
+        private string zipcode;
+        private string street2;
+        private string phonenumber;
+        private string email;
+        private string feedback;
+
+
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                //firstname = value;
+                if (ValidationLibrary.IsItFilledIn(value))
+                {
+                    firstname = value;
+                }
+                else
+                {
+                    firstname = $"ERROR: Please enter first name.";
+                }
+            }
+
+        }
+        public string MiddleName
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                //lastname = value;
+                if (ValidationLibrary.IsItFilledIn(value))
+                {
+                    lastname = value;
+                }
+                else
+                {
+                    lastname = $"\nERROR: Please enter last name.";
+                }
+            }
+        }
+        public string Street1
+        {
+            get
+            {
+                return street1;
+            }
+            set
+            {
+                //street1 = value;
+                if (ValidationLibrary.IsItFilledIn(value))
+                {
+                    street1 = value;
+                }
+                else
+                {
+                    street1 = $"\nERROR: Enter street.";
+                }
+            }
+        }
+        public string Street2
+        {
+            get
+            {
+                return street2;
+            }
+            set
+            {
+                street2 = value;
+ 
+            }
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                //city = value;
+                if (ValidationLibrary.IsItFilledIn(value))
+                {
+                    city = value;
+                }
+                else
+                {
+                    city = $"ERROR: Enter city.";
+                }
+            }
+        }
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+               // state = value;
+               if(ValidationLibrary.ValidateStateTwoLetters(value))
+                {
+                    state = value;
+                }
+               else
+                {
+                    state = $"ERROR: Incorrect Format. Ex. RI";
+                }
+            }
+        }
+        public string ZipCode
+        {
+            get
+            {
+                return zipcode;
+            }
+            set
+            {
+                //zipcode = value;
+                if(ValidationLibrary.ValidateZipCode(value))
+                {
+                    zipcode = value;
+                }
+                else
+                {
+                    zipcode = $"\nERROR: Incorrect Format. Ex: 02910";
+                }
+            }
+        }
+        public string PhoneNumber
+        {
+            get
+            {
+                return phonenumber;
+            }
+            set
+            {
+                //phonenumber = value;
+                if(ValidationLibrary.ValidatePhoneNumber(value))
+                {
+                    phonenumber = value;
+                }
+                else
+                {
+                    phonenumber = $"ERROR: Enter the correct format.";
+                }
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                if (ValidationLibrary.IsValidEmail(value))
+                {
+                    email = value;
+                }
+                else
+                {
+                    email = $"ERROR: Please enter email correctlty";
+                }
+            }
+        }
+        public string Feedback
+        {
+            get
+            {
+                return feedback;
+            }
+            set
+            {
+                    email = value;
+            }
+        }
+
+        public Person()
+        {
+            firstname = "";
+            middlename = "";
+            lastname = "";
+            street1 = "";
+            city = "";
+            state = "";
+            zipcode = "";
+            street2 = "";
+            phonenumber = "";
+            email = "";
+            feedback = "";
+        }
+    }
+}
