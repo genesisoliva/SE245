d3799c78899093ecc839b2b8aca4ae61adc5b658
Genesis <42148976+genesisoliva@users.noreply.github.com>
2022-02-08 18:51:16 -0500

    Update config.php

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,6 @@
 <?php
 
-
-define('DB_HOST', 'localhost');
+define('DB_SERVER', 'localhost');
 define('DB_NAME', 'db_admin');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
