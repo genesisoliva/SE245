98d785f1eb9af4e7616a5481b2327e8028b4e88f
Genesis <42148976+genesisoliva@users.noreply.github.com>
2022-02-08 18:14:59 -0500

    Update Database.php

--- site/lib/Database.php
+++ site/lib/Database.php
@@ -1,25 +1,29 @@
+<?php include "config/config.php"; ?>
 <?php
 
-include "config/config.php";
-
-
-// Class Databse
-
-class  Database{
-
+class  Database {
+  
   public $pdo;
 
-
   // Construct Class
-  public function __construct(){
+  public function __construct() {
 
     if (!isset($this->pdo)) {
+      
       try {
-        $link = new PDO('mysql:host='.DB_HOST.'; dbname='.DB_NAME, DB_USER, DB_PASS);
+        $link = new PDO(  'mysql:host='.DB_SERVER.
+                          ';dbname='.DB_NAME, 
+                          DB_USERNAME, 
+                          DB_PASSWORD
+                       );
+        
         $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $link->exec("SET CHARACTER SET utf8");
+        
         $this->pdo  =  $link;
-      } catch (PDOException $e) {
+      } 
+      
+      catch (PDOException $e) {
         die("Connection error...".$e->getMessage());
       }
 
